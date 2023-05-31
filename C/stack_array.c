3816df9f1f54e27c8897ebe0f4e0d1a843656ed8
Arthurpmrs <arthurpmrs@gmail.com>
2023-05-31 18:40:45 -0300

    Add stack_array implementation in C

---
+++ C/stack_array.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_STACK_SIZE 15
+
+typedef struct StackArray
+{
+    void *items[MAX_STACK_SIZE];
+    int current_size;
+} StackArray;
+
+StackArray *createStackArray()
+{
+    StackArray *newStack = malloc(sizeof(StackArray));
+    newStack->current_size = 0;
+    return newStack;
+}
+
+void push(StackArray *sa, void *item)
+{
+    if (sa->current_size == MAX_STACK_SIZE)
+    {
+        printf("Error: Stack overflow.\n");
+        return;
+    }
+
+    sa->items[sa->current_size++] = item;
+}
+
+void *pop(StackArray *sa)
+{
+    if (sa->current_size == 0)
+    {
+        printf("Error: Stack underflow.\n");
+        return NULL;
+    }
+
+    return sa->items[--sa->current_size];
+}
+
+void *peek(StackArray *sa)
+{
+    if (sa->current_size == 0)
+    {
+        printf("Stack is empty.\n");
+        return NULL;
+    }
+
+    return sa->items[sa->current_size - 1];
+}
+
+void printStackArray(StackArray *sa, void (*printFun)(void *))
+{
+    if (sa->current_size == 0)
+    {
+        printf("Stack is empty.\n");
+        return;
+    }
+
+    printf("TOP\n");
+    for (int i = sa->current_size - 1; i >= 0; i--)
+    {
+        printFun(sa->items[i]);
+        printf("\n");
+    }
+}
+
+// Integer specific functions
+void printInt(void *item)
+{
+    printf("%d", *(int *)item);
+}
+
+int main(void)
+{
+    StackArray *sa = createStackArray();
+    int arr[10] = {45, 87, 96, 78, 12, 1, 0, 47, 3, 5};
+    push(sa, (void *)&arr[0]);
+    push(sa, (void *)&arr[1]);
+    pop(sa);
+    push(sa, (void *)&arr[2]);
+    push(sa, (void *)&arr[3]);
+    push(sa, (void *)&arr[4]);
+    push(sa, (void *)&arr[5]);
+    pop(sa);
+    push(sa, (void *)&arr[6]);
+    pop(sa);
+    push(sa, (void *)&arr[7]);
+    push(sa, (void *)&arr[8]);
+    push(sa, (void *)&arr[9]);
+    pop(sa);
+    pop(sa);
+
+    printf("top=%d\n", *(int *)peek(sa));
+    printStackArray(sa, printInt);
+
+    return 0;
+}
